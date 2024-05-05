afb04e98c566f71fed68c775ee7d90c516051655
Esakki Vinayagam <124988685+EsakkiVinayagam008@users.noreply.github.com>
2024-05-05 18:52:54 +0530

    Update Mainpage.jsx

--- src/Componend/Mainpage.jsx
+++ src/Componend/Mainpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStopwatch,faCalculator } from '@fortawesome/free-solid-svg-icons';
+import { faStopwatch } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 
